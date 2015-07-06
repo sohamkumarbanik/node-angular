7930d8c6b662af4f536bcf78c7793cb7ebb0f93e
root <root@limtexad130130.limtexcal.limtexit.com>
2015-07-06 12:15:36 +0530

    Connected angular to express

--- client/test/spec/controllers/signup.js
+++ client/test/spec/controllers/signup.js
@@ -1,22 +1,20 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: SignupCtrl', function () {
 
   // load the controller's module
   beforeEach(module('clientApp'));
 
-  var MainCtrl,
-    scope;
+  var SignupCtrl;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+    SignupCtrl = $controller('SignupCtrl', {
+      // place here mocked dependencies
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+    expect(SignupCtrl.awesomeThings.length).toBe(3);
   });
 });
